cd0bc9aa0f503a0bb00fe153b4a92c52c2cd4f39
Adrian W <windisch.adrian09@gmail.com>
2023-06-19 16:35:32 +0300

    Refactor main code + builder reusability

--- src/Playground.java
+++ src/Playground.java
@@ -1,18 +1,24 @@
 import DesignPatterns.BaseCar;
+import DesignPatterns.Builder.Car;
 
 public class Playground {
     public static void main(String[] args) {
         System.out.println("Started");
 
-        DesignPatterns.Builder.Car golfGtiBuilder = new DesignPatterns.Builder.Car.Builder("Volkswagen", "Golf GTI").build();
-        DesignPatterns.Builder.Car golfGtiBuilder2 = new DesignPatterns.Builder.Car.Builder("Volkswagen", "Golf GTI")
+        designPatterns();
+    }
+
+    private static void designPatterns() {
+        Car.Builder reusableBuilder = new DesignPatterns.Builder.Car.Builder("Dacia", "Logan"); //Builders are reusable
+
+        DesignPatterns.Builder.Car carBuilder = new DesignPatterns.Builder.Car.Builder("Volkswagen", "Golf GTI")
                 .litreage(2.0f)
                 .doorsNr(5)
                 .fuel(BaseCar.FuelType.PETROL)
                 .power(245)
                 .build();
 
-        System.out.println(golfGtiBuilder2.toString());
+        System.out.println(carBuilder.toString());
 
         DesignPatterns.Javabeans.Car carJavabeans = new DesignPatterns.Javabeans.Car("Audi", "S5");
         carJavabeans.setDoorsNr(3);
